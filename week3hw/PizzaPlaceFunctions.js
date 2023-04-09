68428e59f62156e7df981373f41f6cb93c02191b
Josef Hutton <josefdhutton@gmail.com>
2023-04-09 19:57:05 -0400

    Week3hw

--- week3hw/PizzaPlaceFunctions.js
+++ week3hw/PizzaPlaceFunctions.js
@@ -1,19 +1,40 @@
-let pizzaToppings = ['cheese', 'pepperoni', 'onions', 'bacon'];
-
-//console.log(pizzaToppings);
-
+//create a array of toppings
+const pizzaToppings = ['cheese', 'pepperoni', 'onions', 'bacon'];
 
 
-for (let greeting = 0; greeting < pizzaToppings.length; greeting++) {
-  const element = pizzaToppings[greeting];
-  function greetCustomer() {
-    return "Welcome, these are our toppings, += '$pizzaToppings'"
-  }
+//greet the customer and list toppings
+function greetCustomer() {
+console.log("Welcome, here are our toppings: ")
 }
-console.log(greetCustomer)
-//for (let greeting of pizzaToppings) {
-//  console.log(`"Welcome, these are our toppings, '$pizzaToppings'`)
-//}
+greetCustomer();
+
+for (let i = 0; i < pizzaToppings.length; i++) {
+  console.log(" " + pizzaToppings[i]);
+}
 
 
-//function getPizzaOrder([size, crust, ...toppings]);
+//take pizza order
+function getPizzaOrder(size, crust, ...toppings) {
+  console.log(` Here are two ${size} ${crust} crust pies with ${toppings}.`);
+}
+getPizzaOrder('small', 'thick', 'cheese', 'pepperoni', 'onions', 'bacon');
+
+
+
+//prepare pizza order
+function preparePizza([size, crust, toppings]) {
+  console.log('Pizza cooking! ');
+  let pizza = {
+    size: size,
+    crust: crust,
+    toppings: toppings,
+  };
+}
+preparePizza('small', 'thick', 'cheese', 'pepperoni', 'onions', 'bacon');
+
+
+// serve pizza
+function servePizza(pizza){
+console.log(`"Order up! Here's your ${pizza.size}, ${pizza.crust}, with ${pizza.toppings}, Enjoy!"`)
+}
+servePizza();
