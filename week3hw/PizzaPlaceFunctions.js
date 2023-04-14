c3aa09d90b7dd940eb291ae68a37506b835f5238
Josef Hutton <josefdhutton@gmail.com>
2023-04-13 22:47:36 -0400

    week3hw

--- week3hw/PizzaPlaceFunctions.js
+++ week3hw/PizzaPlaceFunctions.js
@@ -3,39 +3,53 @@ const pizzaToppings = ['cheese', 'pepperoni', 'onions', 'bacon'];
 
 
 //greet the customer and list toppings
-function greetCustomer() {
-console.log("Welcome, here are our toppings: ")
-}
-greetCustomer();
 
-for (let i = 0; i < pizzaToppings.length; i++) {
-  console.log(" " + pizzaToppings[i]);
+function greetCustomer() {
+  let greeting = "Welcome, here are our toppings: ";
+  for (let topping of pizzaToppings) {
+    greeting += `${topping}, `;
+  }
+  console.log(greeting);
 }
+//greetCustomer();
 
 
 //take pizza order
 function getPizzaOrder(size, crust, ...toppings) {
-  console.log(` Here are two ${size} ${crust} crust pies with ${toppings}.`);
+  console.log(`Here are two ${size} ${crust} crust pies with ${toppings} com
+  ing up.`);
+  return [size, crust, toppings];
 }
-getPizzaOrder('small', 'thick', 'cheese', 'pepperoni', 'onions', 'bacon');
+//getPizzaOrder('small', 'thick', 'cheese', 'pepperoni', 'onions', 'bacon');
 
 
 
 //prepare pizza order
-function preparePizza([size, crust, toppings]) {
-  console.log('Pizza cooking! ');
-  let pizza = {
+function preparePizza([size, crust, ...toppings]) {
+  console.log("...Pizza cooking!... ");
+  return {
     size: size,
     crust: crust,
-    toppings: toppings,
+    toppings: toppings
   };
-}
-preparePizza('small', 'thick', 'cheese', 'pepperoni', 'onions', 'bacon');
+};
 
 
-// serve pizza
-function servePizza(pizza){
-console.log(`"Order up! Here's your ${pizza.size}, ${pizza.crust}, with ${pizza.toppings}, Enjoy!"`)
-}
-servePizza();
+
+
+//serve pizza
+function servePizza(pizza) {
+console.log(
+  `Order up! Here's your ${pizza.size} ${pizza.crust} with ${pizza.toppings}, Enjoy!`
+  );
+  return pizza;
+};
+
+
+
+greetCustomer();
+let pizzaOrder = getPizzaOrder('small', 'thick', 'cheese', 'pepperoni', 'onions', 'bacon');
+let prepOrder = preparePizza(pizzaOrder);
+servePizza(prepOrder);
+
 
